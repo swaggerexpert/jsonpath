7e174330eab496ddea2fcacbcdeb5e8662215efe
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-03-23 16:06:59 +0100

    fix: fix evaluator import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as Grammar } from './grammar.js';
 export { default as parse } from './parse/index.js';
-export { default as defaultEvaluator } from './parse/evaluators/default.js';
-
+export { default as translateEvaluator } from './parse/evaluators/translate.js';
