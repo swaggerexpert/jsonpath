fdcc513db1a1eec8c73cb5ebe23a433f67c9fcbd
Vladimir Gorej <vladimir.gorej@gmail.com>
2025-03-24 10:25:06 +0100

    test(parse): add more tests

--- test/parse/cst-corpus.js
+++ test/parse/cst-corpus.js
@@ -6,14 +6,41 @@ import { parse, JSONPathQueryCST } from '../../src/index.js';
 describe('parse', function () {
   context('cst-corpus', function () {
     const jsonPaths = [
+      // https://www.rfc-editor.org/rfc/rfc9535#section-1.4.2
       "$['store']['book'][0]['title']",
       '$.store.book[0].title',
+      // https://www.rfc-editor.org/rfc/rfc9535#section-1.4.3
       '$.store.book[?@.price < 10].title',
+      // https://www.rfc-editor.org/rfc/rfc9535#section-1.5
       '$.store.book[*].author',
       '$..author',
       '$.store.*',
       '$.store..price',
       '$..book[2]',
+      '$..book[2].author',
+      '$..book[2].publisher',
+      '$..book[-1]',
+      '$..book[0,1]',
+      '$..book[:2]',
+      '$..book[?@.isbn]',
+      '$..book[?@.price<10]',
+      '$..*',
+      // https://www.rfc-editor.org/rfc/rfc9535#section-2.2.3
+      '$',
+      // https://www.rfc-editor.org/rfc/rfc9535#section-2.3.1.3
+      "$.o['j j']",
+      "$.o['j j']['k.k']",
+      '$.o["j j"]["k.k"]',
+      `$["'"]["@"]`,
+      // https://www.rfc-editor.org/rfc/rfc9535#section-2.3.2.3
+      '$[*]',
+      '$.o[*]',
+      '$.o[*]',
+      '$.o[*, *]',
+      '$.a[*]',
+      // https://www.rfc-editor.org/rfc/rfc9535#section-2.3.3.3
+      '$[1]',
+      '$[-2]',
     ];
 
     jsonPaths.forEach((jsonPath) => {
