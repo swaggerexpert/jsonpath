39b5b528f490e7e8e8ffeae0223cd4c238684c5d
Vladimir Gorej <vladimir.gorej@gmail.com>
2026-01-16 15:33:02 +0100

    refactor: address CRCs

--- src/parse/trace/Trace.js
+++ src/parse/trace/Trace.js
@@ -6,21 +6,20 @@ class Trace extends BaseTrace {
   inferExpectations() {
     const lines = this.displayTrace().split('\n');
     const expectations = new Set();
-    let collecting = false;
     let lastMatchedIndex = -1;
 
     for (let i = 0; i < lines.length; i++) {
       const line = lines[i];
 
       // capture the max match line (first one that ends in a single character match)
-      if (!collecting && line.includes('M|')) {
+      if (line.includes('M|')) {
         const textMatch = line.match(/]'(.*)'$/);
         if (textMatch && textMatch[1]) {
           lastMatchedIndex = i;
         }
       }
 
-      // begin collecting after the deepest successful match
+      // collect terminal failures after the deepest successful match
       if (i > lastMatchedIndex) {
         const terminalFailMatch = line.match(/N\|\[TLS\(([^)]+)\)]/);
         if (terminalFailMatch) {
